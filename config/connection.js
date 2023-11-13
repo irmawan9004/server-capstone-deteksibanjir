b50575240d1aeb045cc2fa10b9a2bfeb97db5ead
irmawan <irmawan9001@gmail.com>
2023-11-13 23:36:34 +0700

    heroku add databases

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_deteksibanjir", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  "u760614389_db_dteksibnjir",
+  "u760614389_deteksibanjir",
+  "+X9Cz++$ka",
+  {
+    host: "srv1152.hstgr.io",
+    dialect: "mysql",
+  }
+);
 
 export default db;
